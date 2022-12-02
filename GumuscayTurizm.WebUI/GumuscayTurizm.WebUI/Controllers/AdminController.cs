a350e3e60c31af59595346d0f810e3b69920d600
Enver Korkmaz <enverkorkmaz17@gmail.com>
2022-12-02 03:06:38 +0300

    2.12.22

--- GumuscayTurizm.WebUI/GumuscayTurizm.WebUI/Controllers/AdminController.cs
+++ GumuscayTurizm.WebUI/GumuscayTurizm.WebUI/Controllers/AdminController.cs
@@ -2,12 +2,15 @@
 using GumuscayTurizm.Entity;
 using GumuscayTurizm.WebUI.Identity;
 using GumuscayTurizm.WebUI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace GumuscayTurizm.WebUI.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly ICityService _cityService;
@@ -240,11 +243,229 @@ namespace GumuscayTurizm.WebUI.Controllers
         }
         #endregion
 
+        #region User
         public async Task<IActionResult> UserList()
         {
             return View(await _userManager.Users.ToListAsync());
         }
-        
+        public async Task<IActionResult> UserCreate()
+        {
+            var roles = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+            ViewBag.Roles = roles;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserCreate(UserModel userModel, string[] selectedRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                MyIdentityUser user = new MyIdentityUser()
+                {
+                    FirstName = userModel.FirstName,
+                    LastName = userModel.LastName,
+                    UserName = userModel.UserName,
+                    Email = userModel.Email
+                };
+                var result = await _userManager.CreateAsync(user, "Qwe123.");
+                if (result.Succeeded)
+                {
+                    selectedRoles = selectedRoles ?? new string[] { };
+                    await _userManager.AddToRolesAsync(user, selectedRoles);
+                    return RedirectToAction("UserList");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            ViewBag.SelectedRoles = selectedRoles;
+            ViewBag.Roles = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+            return View(userModel);
+        }
+        public async Task<IActionResult> UserEdit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) { return RedirectToAction("UserList"); }
+            var userModel = new UserModel()
+            {
+                UserId = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                UserName = user.UserName,
+                Email = user.Email,
+                SelectedRoles = await _userManager.GetRolesAsync(user)
+            };
+            ViewBag.Roles = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+            return View(userModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(userModel.UserId);
+                if (user != null)
+                {
+                    user.FirstName = userModel.FirstName;
+                    user.LastName = userModel.LastName;
+                    user.UserName = userModel.UserName;
+                    user.Email = userModel.Email;
+                    var result = await _userManager.UpdateAsync(user);
 
+                    if (result.Succeeded)
+                    {
+                        var userRoles = await _userManager.GetRolesAsync(user);
+                        userModel.SelectedRoles = userModel.SelectedRoles ?? new string[] { };
+                        await _userManager.AddToRolesAsync(user, userModel.SelectedRoles.Except(userRoles).ToArray<string>());
+                        await _userManager.RemoveFromRolesAsync(user, userRoles.Except(userModel.SelectedRoles).ToArray<string>());
+                        return RedirectToAction("UserList");
+
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    ViewBag.Roles = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+                    return View(userModel);
+                }
+    
+            }
+            ViewBag.Roles = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+            return View(userModel);
+        }
+        
+        public async Task<IActionResult> ChangeUserPassword(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            ChangePasswordModel changePasswordModel = new ChangePasswordModel() { UserId = user.Id };
+            return View(changePasswordModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangeUserPassword(ChangePasswordModel changePasswordModel)
+        {
+            var user = await _userManager.FindByIdAsync(changePasswordModel.UserId);
+            var userPassToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var result = await _userManager.ResetPasswordAsync(user, userPassToken, changePasswordModel.NewPassword);
+            if (result.Succeeded)
+            {
+               
+                return RedirectToAction("UserList");
+            }
+            return View(changePasswordModel);
+
+        }
+
+        #endregion
+      
+        #region Role
+        public async Task<IActionResult> RoleList()
+        {
+            return View(await _roleManager.Roles.ToListAsync());
+        }
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> RoleCreate(RoleModel roleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole() { Name = roleModel.Name };
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("RoleList");
+                }
+            }
+            return View(roleModel);
+        }
+        public async Task<IActionResult> RoleEdit(string id)
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var role = await _roleManager.FindByIdAsync(id);
+            var members = new List<MyIdentityUser>();
+            var nonMembers = new List<MyIdentityUser>();
+            foreach (var user in users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+               
+            }
+            RoleDetails roleDetails = new RoleDetails()
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            };
+            return View(roleDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RoleEdit(RoleEditModel roleEditModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                foreach (var userId in roleEditModel.IdsToAdd ?? new string[] { })
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        var result = await _userManager.AddToRoleAsync(user, roleEditModel.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+
+                
+                foreach (var userId in roleEditModel.IdsToRemove ?? new string[] { })
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        var result = await _userManager.RemoveFromRoleAsync(user, roleEditModel.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+
+            }
+            return Redirect($"/Admin/RoleEdit/{roleEditModel.RoleId}");
+        }
+
+        public async Task<IActionResult> RoleDelete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null) { return NotFound(); }
+            foreach (var user in await _userManager.Users.ToListAsync())
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                   
+                    return RedirectToAction("RoleList");
+                }
+            }
+            var result = await _roleManager.DeleteAsync(role);
+            if (result.Succeeded)
+            {
+
+            }
+            return RedirectToAction("RoleList");
+        }
+
+        #endregion
+
+        
     }
 }
---
+++ GumuscayTurizm.WebUI/GumuscayTurizm.WebUI/Models/ChangePasswordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GumuscayTurizm.WebUI.Models
+{
+    public class ChangePasswordModel
+    {
+        public string UserId { get; set; }
+        public string NewPassword { get; set; }
+        public string ReNewPassword { get; set; }
+    }
+}
